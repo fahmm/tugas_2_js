f623149b55d1edb1af98b59e7d5c9f40233fa47b
fahmi <pammimajid@gmail.com>
2020-03-10 21:19:36 +0800

    ok

---
+++ tugas_2_js.js
@@ -0,0 +1,21 @@
+
+var number = 20;
+var huruf="tes dulu";
+var umur = 17;
+if (umur > 18)
+umur = true;
+else
+umur = false;
+var kosong =null;
+var angka =[5,6,1,3,7];
+
+
+console.log(number);
+console.log(huruf);
+console.log(umur);
+console.log(kosong);
+console.log(angka[4]);
+
+
+
+
